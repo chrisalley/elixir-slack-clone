4e0509e91e1abeb809e00af350da290f875b9882
Chris Alley <chris@chrisalley.info>
2017-02-19 20:16:29 +1100

    Setup User model in Phoenix

--- api/mix.exs
+++ api/mix.exs
@@ -19,7 +19,7 @@ defmodule SlackClone.Mixfile do
   def application do
     [mod: {SlackClone, []},
      applications: [:phoenix, :phoenix_pubsub, :cowboy, :logger, :gettext,
-                    :phoenix_ecto, :postgrex]]
+                    :phoenix_ecto, :postgrex, :comeonin]]
   end
 
   # Specifies which paths to compile per environment.
@@ -35,7 +35,8 @@ defmodule SlackClone.Mixfile do
      {:phoenix_ecto, "~> 3.0"},
      {:postgrex, ">= 0.0.0"},
      {:gettext, "~> 0.11"},
-     {:cowboy, "~> 1.0"}]
+     {:cowboy, "~> 1.0"},
+     {:comeonin, "~> 2.5"}]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
