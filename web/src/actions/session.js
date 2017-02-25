54dce8e6424800d456a483b48b658fa0f729b3ac
Chris Alley <chris@chrisalley.info>
2017-02-26 00:06:01 +1100

    Pesist sessions and protect routes on the frontend

--- web/src/actions/session.js
+++ web/src/actions/session.js
@@ -33,3 +33,19 @@ export function logout(router) {
       router.transitionTo('/login');
     });
 }
+
+export function authenticate() {
+  return (dispatch) => {
+    dispatch({ type: 'AUTHENTICATION_REQUEST' });
+    return api.post('/sessions/refresh')
+      .then((response) => {
+        setCurrentUser(dispatch, response);
+      })
+      .catch(() => {
+        localStorage.removeItem('token');
+        window.location = '/login';
+      });
+  };
+}
+
+export const unauthenticate = () => ({ type: 'AUTHENTICATION_FAILURE' });
